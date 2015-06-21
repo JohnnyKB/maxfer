cc63f985b8cf9904eb0105aacec7a8f5a61eb9c4
Johnny Kutnowski <j.kutnob@gmail.com>
2015-06-22 00:40:38 +0200

    Cards near final

--- js/main.js
+++ js/main.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
 	$('.navmenu__item').on('click', function(){
 		$(this).toggleClass('active');
 	});
+	$('.feature').on('click', function(){
+		if ($(this).siblings().hasClass('active')) {
+			$(this).siblings().removeClass('active');
+			$(this).toggleClass('active');
+		} else {
+			$(this).toggleClass('active');
+		}
+	});
 });
