b9101070aff37d10cc98db534bff8692b42c941b
JohnnyKB <j.kutnob@gmail.com>
2015-06-30 01:14:17 +0200

    Added resposnive menu

--- js/main.js
+++ js/main.js
@@ -10,16 +10,28 @@ $(document).ready(function(){
 			$(this).toggleClass('active');
 		}
 	});
-    $('.first-feature').bind('inview', function(event, visible) {
-      if (visible) {
-        $(this).addClass('inview');
-        var k = 1;
-        $(this).siblings('.feature').each(function(i, val){
-        	setTimeout(function(){
-        		$(val).addClass('inview');
-        	}, k*125);
-        	k++;
-        });
-      }
-    });
+	$('.first-feature').bind('inview', function(event, visible) {
+		if (visible) {
+			$(this).addClass('inview');
+			var k = 1;
+			$(this).siblings('.feature').each(function(i, val){
+				setTimeout(function(){
+					$(val).addClass('inview');
+				}, k*125);
+				k++;
+			});
+		}
+	});
+$("#offmenu").mmenu({
+		"slidingSubmenus": false,
+		"offCanvas": {
+		"position": "right"
+	},
+	"extensions": [
+		"effect-slide",
+		"pageshadow",
+		"theme-white",
+		"border-full"
+		]
+	});
 });
