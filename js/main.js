c4073927efdbf29e6b6f68f97186538cf8df9708
Johnny Kutnowski <j.kutnob@gmail.com>
2015-07-01 02:49:44 +0200

    Offcanvas menu is done. Further enhancements

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
+
+	// Returns a function, that, as long as it continues to be invoked, will not
+	// be triggered. The function will be called after it stops being called for
+	// N milliseconds. If `immediate` is passed, trigger the function on the
+	// leading edge, instead of the trailing.
+	debounce = function(func, wait, immediate) {
+		var timeout;
+		return function() {
+			var context = this, args = arguments;
+			var later = function() {
+				timeout = null;
+				if (!immediate) func.apply(context, args);
+			};
+			var callNow = immediate && !timeout;
+			clearTimeout(timeout);
+			timeout = setTimeout(later, wait);
+			if (callNow) func.apply(context, args);
+		};
+	};
+
 	$('.navmenu__item').on('click', function(){
 		$(this).toggleClass('active');
 	});
@@ -20,9 +40,10 @@ $(document).ready(function(){
 				}, k*125);
 				k++;
 			});
+			$(this).unbind('inview');
 		}
 	});
-$("#offmenu").mmenu({
+	$("#offmenu").mmenu({
 		"slidingSubmenus": false,
 		"offCanvas": {
 		"position": "right"
@@ -34,4 +55,13 @@ $("#offmenu").mmenu({
 		"border-full"
 		]
 	});
+
+	// var reloadSource = debounce(function(e) {
+	// 	var www = $(window).width();
+	// 	$('.target').attr('src', 'http://placehold.it/'+www+'x470')
+	// }, 150, false);
+
+	// $(window).on('resize', reloadSource);
+
+	
 });
