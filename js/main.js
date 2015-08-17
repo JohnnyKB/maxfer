eb90a24414def62231de682734b665ac220b8f23
Johnny Kutnowski <j.kutnob@gmail.com>
2015-08-17 20:35:04 +0200

    Different subpages loading

--- js/main.js
+++ js/main.js
@@ -1,27 +1,41 @@
-$(document).ready(function(){
+var target = $('.target');
 
-	// Returns a function, that, as long as it continues to be invoked, will not
-	// be triggered. The function will be called after it stops being called for
-	// N milliseconds. If `immediate` is passed, trigger the function on the
-	// leading edge, instead of the trailing.
-	debounce = function(func, wait, immediate) {
-		var timeout;
-		return function() {
-			var context = this, args = arguments;
-			var later = function() {
-				timeout = null;
-				if (!immediate) func.apply(context, args);
-			};
-			var callNow = immediate && !timeout;
-			clearTimeout(timeout);
-			timeout = setTimeout(later, wait);
-			if (callNow) func.apply(context, args);
-		};
-	};
+$(document).ready(function(){
+	
+	startPage();
+	loadOffmenu();
 
 	$('.navmenu__item').on('click', function(){
 		$(this).toggleClass('active');
 	});
+
+	$('.trigger').on('click', loadPage);
+});
+
+function loadPage () {
+	var page = $(this).data('page');
+	if (!$('body').hasClass(page+'-open')){
+		target.slideToggle(function(){
+			target.load('page--' + page + '.html', function(){
+				if (page == 'ventajas') {
+					ventajasPage();
+				}
+				target.slideToggle();
+			})
+		});
+		$('body').toggleClass(page+'-open');
+	}
+}
+
+function startPage() {
+	target.load('page--start.html', function(){
+		setTimeout(function(){
+			target.slideToggle();
+		},1000)
+	});
+}
+
+function ventajasPage () {
 	$('.feature').on('click', function(){
 		if ($(this).siblings().hasClass('active')) {
 			$(this).siblings().removeClass('active');
@@ -43,6 +57,9 @@ $(document).ready(function(){
 			$(this).unbind('inview');
 		}
 	});
+}
+
+function loadOffmenu () {
 	$("#offmenu").mmenu({
 		"slidingSubmenus": false,
 		"offCanvas": {
@@ -55,4 +72,23 @@ $(document).ready(function(){
 		"border-full"
 		]
 	});
-});
+}
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
